268fd9028176eb07be668a665030842f30d0f78e
JoelBot <Joel.Lonneker@gmail.com>
2016-11-03 16:23:20 -0400

    components filled in

--- components/Signin.js
+++ components/Signin.js
@@ -1,1 +1,13 @@
 import React from 'react'
+
+class Signin extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+    render() {
+        return <h1> TEST FROM Signin </h1>
+    }
+}
+
+export default Signin
--- components/Signup.js
+++ components/Signup.js
@@ -1,1 +1,13 @@
 import React from 'react'
+
+class Signup extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+    render() {
+        return <h1> TEST FROM Signup </h1>
+    }
+}
+
+export default Signup
