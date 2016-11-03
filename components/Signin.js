c4fdfcfc013b30e32957838b221ea01b9b7f48b0
Manpreet Singh <mannyy91s@gmail.com>
2016-11-03 17:34:43 -0400

    moved signin over to react component.

--- components/Signin.js
+++ components/Signin.js
@@ -6,7 +6,27 @@ class Signin extends React.Component {
 
     }
     render() {
-        return <h1> TEST FROM Signin </h1>
+        return  <div className="row">
+                <div className="col-sm-6 col-sm-offset-3">
+                    <div className="form-group">
+                    <label>Email</label>
+                    <input id="emailSignIn" type="text" name="email" className="form-control" required/>
+                  </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input id="forgotPassword" type="password" className="password" className="form-control" required/>
+                </div>
+                <div className="row">
+                  <div className="col-sm-6">
+                    <a href="#">Forget Password?</a>
+                  </div>
+                  <div className="col-sm-6 text-right">
+                    <button id="login" type="button" className="btn btn-success">Login</button>
+                  </div>
+                </div>
+              </div>
+            </div>
     }
 }
 
