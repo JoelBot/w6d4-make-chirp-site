5c08859d762067c7f9559b6c9b7611cbcb1d7904
Manpreet Singh <mannyy91s@gmail.com>
2016-11-03 17:20:30 -0400

    moved stuff over to react home component and added links.

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router'
+
 
 class Home extends React.Component {
     constructor(props) {
         super(props)
 
     }
+
     render() {
-        return <h1> TEST FROM HOME </h1>
+        return   <div id="homePage" className="row">
+                    <div className="col-sm-12 text-center">
+                        <img src="./img/chirp-logo.png" alt="Logo Pic" />
+                    </div>
+                    <div className="col-sm-6 text-center">
+                        <Link to="/signin">
+                        <button id="signin" type="button" className="btn btn-primary btn-block" >SignIn</button>
+                        </Link>
+                    </div>
+                    <div className="col-sm-6">
+                        <Link to="/signup">
+                        <button id="signup" type="button" className="btn btn-success btn-block" >SignUp</button>
+                        </Link>
+                    </div>
+                    </div>
     }
 }
 
