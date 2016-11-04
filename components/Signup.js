19c4bfaf295abb55a124b4f239b384557751a5b5
JoelBot <Joel.Lonneker@gmail.com>
2016-11-04 11:37:52 -0400

    signup API fetch returning token and storing to session storage

--- components/Signup.js
+++ components/Signup.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Signup extends React.Component {
     constructor(props) {
         super(props)
-        this.signUpPost = this.signUpPost.bind(this)
+        this.signupPost = this.signupPost.bind(this)
         this.signedupHandler = this.signedupHandler.bind(this)
         this.state =  {
             name: '',
@@ -13,53 +13,51 @@ class Signup extends React.Component {
             }
     }
 
-    signUpPost() {
-        var data = new FormData()  // Check if append is needed here and place API calls here.
+    signupPost() {
+        // var data = new FormData()  // Check if append is needed here and place API calls here.
         // data.append('name', this.state.name)
-        data.name = this.state.name
-        data.email = this.state.email
-        data.password = this.state.password
-        data.avatar = this.state.avatar
+        // data.name = this.state.name
+        // data.email = this.state.email
+        // data.password = this.state.password
+        // data.avatar = this.state.avatar
         // data.append('email', this.state.email)
         // data.append('password', this.state.password)
         // data.append('avatar', this.state.avatar)
-        console.log(data)
+        // console.log(data)
 //
-        // fetch('http://f5ebc883.ngrok.io/users', { // Update this
-        //     body: JSON.stringify({
-        //         name: data.name,
-        //         email: data.email,
-        //         password: data.password,
-        //         avatar: data.avatar
-        //     }),
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-        // })
-        // .then(response => response.json())
-        // .then(signedupHandler)
+        fetch('http://f5ebc883.ngrok.io/users', { // Update this
+            body: JSON.stringify({
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                avatar: this.state.avatar
+            }),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(this.signedupHandler)
     }
     signedupHandler(response) {
-    //     // console.log(response) // this to see the token
-    //     if {typeof response.user != 'undefined') {
-    //         sessionStorage.setItem('phetchly', response.user.api_token)
-    //
-    //         window.location.href = '/home' // redirects after login update to displaying chirp
-    //     }}
-    //     else {
-    //         response.forEach(function(error) {
-    //             var errorDiv = document.createElement('div')
-    //             errorDiv.classList.add('alert', 'alert-danger')
-    //             errorDiv.innerHTML = error
-    //             document.querySelector('#errors').appendChild(errorDiv)
-    //         })
-    //     }
-    // }
+        console.log(response) // this to see the token
+        if (typeof response.user != 'undefined') {
+            sessionStorage.setItem('chirp_token', response.user.api_token)
 
-    //
-
+            window.location.href = '/' // redirects after login update to displaying chirp
+        }
+        else {
+            response.forEach(function(error) {
+                var errorDiv = document.createElement('div')
+                errorDiv.classList.add('alert', 'alert-danger')
+                errorDiv.innerHTML = error
+                document.querySelector('#errors').appendChild(errorDiv)
+            })
+        }
     }
+
+    //
 
     render() {
         return <div>
@@ -80,12 +78,12 @@ class Signup extends React.Component {
         </div>
         <div className="form-group">
         <label htmlFor="avatar">Avatar</label>
-        <input type="text" id="avatar" name="avatar" className="form-control" required onChange={(e) => this.setState({avatar:e.target.value})}/>
+        <input type="file" id="avatar" name="avatar" className="form-control" required onChange={(e) => this.setState({avatar:e.target.files[0]})}/>
         </div>
         <div className="form-group">
         <div className="row">
         <div className="col-sm-6">
-        <button type="button" onClick={this.signUpPost} id="signup" className="btn btn-success btn-block" >Signup!</button>
+        <button type="button" onClick={this.signupPost} id="signup" className="btn btn-success btn-block" >Signup!</button>
         </div>
         <div className="col-sm-6">
         <button type="button" id="cancelSignup" className="btn btn-primary btn-block">Cancel</button>
