edc92da7aaeaf676903d94b32393ab06d4fe42fd
Manpreet Singh <mannyy91s@gmail.com>
2016-11-04 11:36:35 -0400

    added fetch and api token for signin.

--- components/Signin.js
+++ components/Signin.js
@@ -3,35 +3,51 @@ import React from 'react'
 class Signin extends React.Component {
     constructor(props) {
         super(props)
-        this.logIn = this.logIn.bind(this)
+        this.signinHandler = this.signinHandler.bind(this)
+        this.signedinHandler = this.signedinHandler.bind(this)
         this.state = {
             email: '',
             password: ''
         }
     }
-    logIn() {
-      var data = new FormData()
-      data.append('email', this.state.email)
-      data.append('password', this.state.password)
+    signinHandler() {
+      fetch('http://f5ebc883.ngrok.io/log_in', {
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password
+        }),
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(this.signedinHandler)
+    }
+    signedinHandler(response) {
+      sessionStorage.setItem('chirp_token', response.user.api_token)
+      window.location.href = '/chirp.html'
+    //   console.log(response)
     }
     render() {
+        console.log(this.state.email)
         return  <div className="row">
                 <div className="col-sm-6 col-sm-offset-3">
                     <div className="form-group">
                     <label>Email</label>
-                    <input id="emailSignIn" type="email" name="email"  onChange={(e)=>this.setState({email:e.target.value})} className="form-control" required/>
+                    <input id="signinEmail" type="email" name="email"  onChange={(e) => this.setState({email:e.target.value})} className="form-control" required/>
                   </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input id="forgotPassword" type="password"  onChange={(e)=>this.setState({password:e.target.value})} className="password" className="form-control" required/>
+                    <input id="signinPassword" type="password"  onChange={(e) => this.setState({password:e.target.value})} className="password" className="form-control" required/>
                 </div>
                 <div className="row">
                   <div className="col-sm-6">
                     <a href="#">Forget Password?</a>
                   </div>
                   <div className="col-sm-6 text-right">
-                    <button id="login" type="button" className="btn btn-success" onClick={this.logIn}>Login</button>
+                    <button id="login" type="button" className="btn btn-success" onClick={this.signinHandler}>Login</button>
                   </div>
                 </div>
               </div>
