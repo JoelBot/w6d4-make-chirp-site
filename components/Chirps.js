70c4ed97e13d99f83f1e2b35d3a433c0b4e42716
Manpreet Singh <mannyy91s@gmail.com>
2016-11-05 12:36:31 -0400

    test

--- components/Chirps.js
+++ components/Chirps.js
@@ -7,8 +7,6 @@ class Chirps extends Component {
         this.typing = this.typing.bind(this) // This is boilerplate stuff for react that binds this
         this.enter = this.enter.bind(this)
         this.updateChirps = this.updateChirps.bind(this)
-        // this.setLocalStorage = this.setLocalStorage.bind(this)
-
         this.state = {
             newChirp: '',
             chirps: []
@@ -18,7 +16,6 @@ class Chirps extends Component {
         fetch('https://nameless-cove-75673.herokuapp.com/posts')
         .then(response => response.json())
         .then(response => this.setState({chirps: response.posts}))  // Sets state each API chirp get from /posts.  will need to post all chirps to backend so this will pick them up and display them.
-
     }
 
     typing(e) {
@@ -58,8 +55,8 @@ class Chirps extends Component {
     }
 
     render() {
-        // var ChirpItems = this.state.chirps.map((chirp, i) => {
-        //     return <Chirp data={chirp} key={i}/>})
+        var ChirpItems = this.state.chirps.map((chirp, i) => {
+            return <Chirp data={chirp} key={i}/>})
 
         return (
             <div className="input-group form-control">
